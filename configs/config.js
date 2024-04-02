9998fc22a61d1b4884b37f4f60319f783f780690
hangnguyen118 <hangnguyen118.wy@gmail.com>
2024-04-02 10:49:51 +0700

    update

---
+++ configs/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SECRET_KEY: 'TUNG123',
+    EXPIRE_JWT: '1d',
+    hostName: 'localhost:3000',
+    Host: "sandbox.smtp.mailtrap.io",
+    Port: 2525,
+    Username: "a46e580822599e",
+    Password: "ddc6aeff884d20"
+}
